e42e7e911e3788628783b7a4e367a30e5f73772a
Steven Scaffidi <sscaff1@gmail.com>
2015-10-27 11:16:35 -0700

    updating the package

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'sscaff1:semantic-messages',
-  version: '0.0.1',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'A simple package to display flash messages using semantic-ui',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/sscaff1/semantic-messages.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -13,7 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.0.2');
   api.use([
-    'semantic:semantic-ui',
+    'semantic:ui@2.1.4_5',
     'minimongo',
     'mongo-livedata',
     'templating'
